1dd1375f070fed5aa27d2763217d8c36458ab8e8
htdat0704 <hodat402@gmail.com>
2023-02-07 09:41:51 +0700

    read and write QR

--- my-server/index.js
+++ my-server/index.js
@@ -2,25 +2,71 @@
 const express = require("express");
 const app = express();
 const QRCode = require('qrcode');
+// __ Importing jimp __ \\
+const Jimp = require("jimp");
+
+// __ Importing filesystem = __ \\
+const fs = require('fs')
+
+// __ Importing qrcode-reader __ \\
+const qrCodeReader = require('qrcode-reader');
 
 const auth = require("./route");
 
 app.use("/auth", auth);
 
-// QRCode.toFile('/output-file-path/file.png', 'Encode this text in QR code', {
-//     errorCorrectionLevel: 'H'
-//   }, function(err) {
-//     if (err) throw err;
-//     console.log('QR code saved!');
-//   });
+let data = {
+  apartment: "404",
+  name: "Long",
+  relative: "owner"
+}
+const myJSON = JSON.stringify(data);
+let wait = async (ms) => {
+  return new Promise(r => setTimeout(r, ms));
+}
+let toFile = () => QRCode.toFile('my-server/file.png',` ${myJSON}`, {
+  errorCorrectionLevel: 'H'
+}, function(err) {
+  if (err) throw err;
+  console.log('QR code saved!');
+});
 
-QRCode.toString('Encode this text in QR code', {
-    errorCorrectionLevel: 'H',
-    type: 'svg'
-  }, function(err, data) {
-    if (err) throw err;
-    console.log(data);
+const read = () => {
+  const buffer = fs.readFileSync('my-server/file.png') 
+  Jimp.read(buffer, (err, image) =>  {
+    if (err) {
+        console.error(err);
+    }
+  // __ Creating an instance of qrcode-reader __ \\
+    const qrCodeInstance = new qrCodeReader();
+  
+    qrCodeInstance.callback = function(err, value) {
+        if (err) {
+            console.error(err);
+        }
+  // __ Printing the decrypted value __ \\
+        console.log(value.result);
+    };
+  
+  // __ Decoding the QR code __ \\
+    qrCodeInstance.decode(image.bitmap);
   });
+}
+
+const writeAndRead = async() => {
+  toFile();
+  await wait(2016);
+  read();
+}
+
+writeAndRead()
+// QRCode.toString('Encode this text in QR code', {
+//     errorCorrectionLevel: 'H',
+//     type: 'svg'
+//   }, function(err, data) {
+//     if (err) throw err;
+//     console.log(data);
+//   });
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
 console.log(`Listening to port ${port}...`);
